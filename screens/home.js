deec543ae585a27db240ccc4ae582956f05d5bda
Tecnodix2020 <tecnodix2020@gmail.com>
2020-09-13 22:45:13 -0300

    Home Screen alterada para responsiva

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, Image, TouchableWithoutFeedback } from 'react-native';
+import { PixelRatio, Dimensions, StyleSheet, View, Text, Button, Image, TouchableWithoutFeedback } from 'react-native';
+
+var FONT_LOGO = 26;
+
+if (PixelRatio.get() <= 1.5) {
+  FONT_LOGO = 18;
+}
+
+
 
 export default function Home({ navigation }) {
     
@@ -25,28 +33,26 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     backgroundColor: '#50C3F3',
-    justifyContent: 'center',
     alignItems: 'center',
     alignContent: 'center',
+    justifyContent: 'center',
   },
   placeholderLogo: {
     backgroundColor: '#FFFFFF',
-    flex: 1,
-    width: 300,
-    height: 300,
-    borderRadius: 300/2,
+    width: Dimensions.get('window').width * 0.32,
+    height: Dimensions.get('window').width * 0.32,
+    borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
     justifyContent: 'center',
     alignItems: 'center',
-    alignContent: 'center',
-    margin: '12%'
   },
   imgLogo: {
-    width: 170,
-    height: 150,
+    width: '60%',
+    height: '50%',
     margin: 10,
+    resizeMode: 'stretch',
   },
   txtLogo: {
-    fontSize: 35,
+    fontSize: FONT_LOGO,
     color: 'red',
   }
 });
